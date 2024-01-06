8295646814902d822ff13f98831b4ffc332ebd09
Lagzen Thakuri <thakurizen2@gmail.com>
2024-01-06 21:58:38 +0545

    Update authSchema validation function.

--- middleware/validateMiddleware.js
+++ middleware/validateMiddleware.js
@@ -1,8 +1,8 @@
-const authSchema = require("../model/authSchema")
+    const authSchema = require("../model/authSchema")
 
-  const  validationMiddleware= async(req,res,next) =>{
-    const result = await authSchema.validateAsync(req.body)
-    console.log(result)
-    next(); 
-}
-module.exports= validationMiddleware
+    const  validationMiddleware= async(req,res,next) =>{
+        const result = await authSchema(req.body)
+        console.log(result)
+        next(); 
+    }
+    module.exports= validationMiddleware
